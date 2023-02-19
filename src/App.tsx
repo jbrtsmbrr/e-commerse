fed3781377bb4dfff76ce417a0c6028603487dde
Joebert <jlsembrero@findme.com.ph>
2023-02-20 00:33:56 +0800

    added: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { useAuthContext } from "./context/Auth/Auth.provider";
+import Login from "./pages/Login";
+import AppLayout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+
+const Products = () => <div>Products</div>
+const NotFound = () => <div>404</div>
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const { user } = useAuthContext();
+  console.log(user)
+  return <Routes>
+    {user ? (
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/products" element={<Products />} />
+        <Route path="/home" element={<Home />}/>
+      </Route>
+    ) : (
+      <Route path="/login" element={<Login />} />
+    )}
+    <Route path="*" element={<NotFound />} />
+  </Routes>
 }
 
 export default App
