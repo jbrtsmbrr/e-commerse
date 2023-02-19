ffd6018254daf6a1eb103cc85dd939b6cac42709
Joebert <jlsembrero@findme.com.ph>
2023-02-20 00:33:23 +0800

    added: setup route and context providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/Auth/Auth.provider";
+import ShoppingProvider from './context/Shopping/Shopping.provider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <ShoppingProvider>
+          <App />
+        </ShoppingProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
