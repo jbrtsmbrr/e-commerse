c82e241c2ee2d09ff77caa594d4559fd585d50c3
Joebert <jlsembrero@findme.com.ph>
2023-03-07 20:50:51 +0800

    feat: add support for keyword debouncing

--- src/context/Shopping/Shopping.provider.tsx
+++ src/context/Shopping/Shopping.provider.tsx
@@ -5,15 +5,19 @@ interface IShoppingProviderProps {
   children: React.ReactNode
 }
 
-type Cart = {
+export type Cart = {
   product: Product,
   quantity: number
 }
 type ShoppingContextType = {
+  keyword: string
+  onKeywordChanged: (nextValue: string) => void
   cart: Cart[] | null
   wishList: Product[] | null
   addToCart: (product: Product) => void,
-  getCartCount: () => number
+  getCartCount: () => number,
+  decreaseIQty: (product: Product) => void,
+  increaseIQty: (product: Product) => void
 }
 
 const ShoppingContext = createContext({} as ShoppingContextType);
@@ -29,6 +33,7 @@ export const useShoppingContext = () => {
 const ShoppingProvider: React.FC<IShoppingProviderProps> = ({ children }) => {
   const [cartItems, setCartItems] = useState<Cart[]>([]);
   const [wishListItems, setWishlistItems] = useState<Product[] | null>(null);
+  const [keyword, setKeyword] = useState("");
 
   const addToCart = (product: Product) => {
     const productIndex = cartItems.findIndex(cartItem => cartItem.product.id === product.id);
@@ -50,8 +55,36 @@ const ShoppingProvider: React.FC<IShoppingProviderProps> = ({ children }) => {
 
   const getCartCount = () => cartItems.reduce((accum, current) => accum += current.quantity, 0)
 
+  const decreaseIQty = (product: Product) => {
+    setCartItems((items) => {
+      const index = items.findIndex(i => i.product.id === product.id);
+      const itemsInstance = [...items];
+      const nextValue = itemsInstance[index].quantity - 1
+      if (nextValue < 1) return itemsInstance;
+      itemsInstance[index].quantity -= 1;
+
+      return itemsInstance;
+    })
+  };
+
+  const increaseIQty = (product: Product) => {
+    setCartItems(items => {
+      const index = items.findIndex(i => i.product.id === product.id);
+      const itemsInstance = [...items];
+      itemsInstance[index].quantity += 1;
+      return itemsInstance;
+    })
+  }
+
+  const onKeywordChanged = (nextValue: string) => {
+    setKeyword(nextValue);
+  }
+
+
+  const contextValue: ShoppingContextType = { keyword, onKeywordChanged, cart: cartItems, wishList: wishListItems, addToCart, getCartCount, decreaseIQty, increaseIQty }
+
   return (
-    <ShoppingContext.Provider value={{ cart: cartItems, wishList: wishListItems, addToCart, getCartCount }}>
+    <ShoppingContext.Provider value={contextValue}>
       {children}
     </ShoppingContext.Provider>
   )
