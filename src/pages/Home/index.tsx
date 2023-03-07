5f087a8b3e6814afb1c6ebbd242275a385f68a03
Joebert <jlsembrero@findme.com.ph>
2023-03-07 20:51:53 +0800

    feat: add support for keyword filter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Grid, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import ShopCard, { Product } from '../../components/ShopCard'
+import { useShoppingContext } from '../../context/Shopping/Shopping.provider';
 
 const useFetch = (url: string) => {
   const [data, setData] = useState<Product[] | null>(null);
@@ -22,14 +23,16 @@ const useFetch = (url: string) => {
 }
 
 const Home = () => {
+  const { keyword } = useShoppingContext();
   const { data: recentlyData, loading: recenylyLoading } = useFetch('https://fakestoreapi.com/products?limit=5&sort=desc');
   const { data, loading } = useFetch('https://fakestoreapi.com/products?limit=10');
+
   return (
     <div style={{ width: "85%", margin: "auto auto" }}>
       <div style={{ marginTop: 40, paddingInline: 15 }}>
         <Typography variant="h6" color="black" marginLeft={1} marginBottom={2} fontWeight={600}>Recently viewed</Typography>
         <Grid container gap={3} justifyContent="center" alignItems="center">
-          {recentlyData?.map((product) =>
+          {recentlyData?.filter(p => p.title.toLowerCase().includes(keyword.toLowerCase())).map((product) =>
             <ShopCard product={product} />
           )}
         </Grid>
@@ -37,7 +40,7 @@ const Home = () => {
       <div style={{ marginTop: 40, paddingInline: 15 }}>
         <Typography variant="h6" color="black" marginLeft={1} marginBottom={2} fontWeight={600}>New Products</Typography>
         <Grid container gap={3} justifyContent="center" alignItems="center">
-          {data?.map((product) =>
+          {data?.filter(p => p.title.toLowerCase().includes(keyword.toLowerCase())).map((product) =>
             <ShopCard product={product} />
           )}
         </Grid>
